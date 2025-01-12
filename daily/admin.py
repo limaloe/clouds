331ac5127e57d8320201c29d9a3a0ce78e2ff218
limaloe <spring112time@gmail.com>
2025-01-12 18:33:06 +1000

    Untrack files in .gitignore

--- daily/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created', 'published']
-    prepopulated_fields = {'slug': ('title',)}
-admin.site.register(Post, PostAdmin)
